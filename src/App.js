c0c2f8550458f15b572099e3a6bb10fdd7b7eada
Simon Ingeson <44818+smonn@users.noreply.github.com>
2019-07-18 23:28:29 -0400

    Add h1 to app component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1><img src={logo} className="App-logo" alt="logo" /></h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>API_URL = {window._env_.API_URL}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
       </header>
     </div>
   );
